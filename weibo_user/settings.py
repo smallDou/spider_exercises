b4cab5a268d7a9c18fe4867f69f366aa437c226e
smallDou <1411605350@qq.com>
2018-05-16 00:57:16 +0800

    配置文件

--- weibo_user/settings.py
+++ weibo_user/settings.py
@@ -1,3 +1,21 @@
 ## 登录
-USERNAME = '17192198543'
-PASSWORD = 'LXQ199710071234'
+USERNAME = ''
+PASSWORD = ''
+
+##redis地址和端口,用于储存cookie
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_PASSWORD = None
+
+##mongo地址和端口，用于存储数据
+MONGO_HOST = 'localhost'
+MONGO_PORT = 27017
+
+##headers
+User_Agent = [
+            'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 '
+            'Mobile/13B143 Safari/601.1]',
+            'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) '
+            'Chrome/48.0.2564.23 Mobile Safari/537.36',
+            'Mozilla/5.0 (Linux; Android 5.1.1; Nexus 6 Build/LYZ28E) AppleWebKit/537.36 (KHTML, like Gecko) '
+            'Chrome/48.0.2564.23 Mobile Safari/537.36']
