e52197907841348a78fc70a11718598389f26e8f
smallDou <1411605350@qq.com>
2018-05-16 00:57:25 +0800

    程序入口

--- weibo_user/run.py
+++ weibo_user/run.py
@@ -1,8 +1,9 @@
-from login import Login
+from spider import Spider
 
 def main():
-    l = Login()
-    l.save_cookies()
+    s = Spider.verify_cookie()
+    s.get_weibo_info()
 
+#设置定时任务
 if __name__ == '__main__':
     main()
