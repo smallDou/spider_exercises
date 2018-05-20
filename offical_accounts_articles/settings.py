a9c1d40d18f71f7a22d68ab1ed207eb2b68b1f97
smallDou <1411605350@qq.com>
2018-05-20 15:54:59 +0800

    配置文件

---
+++ offical_accounts_articles/settings.py
@@ -0,0 +1,15 @@
+## 登录,必须有微信公众号账号，没有可以去https://mp.weixin.qq.com/注册订阅号
+USERNAME = ''
+PASSWORD = ''
+
+##redis地址和端口,用于储存cookie
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_PASSWORD = None
+
+##mongo地址和端口，用于存储数据
+MONGO_HOST = 'localhost'
+MONGO_PORT = 27017
+
+##公众号名称
+query = '一个程序员的日常'
